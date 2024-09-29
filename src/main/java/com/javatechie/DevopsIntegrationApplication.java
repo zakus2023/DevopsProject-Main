ea5d946e9448b71de81a10a3db2443c4c4cd64fb
abdul <idbsch2012@gmail.com>
2024-09-28 21:20:52 -0400

    first commit

--- src/main/java/com/javatechie/DevopsIntegrationApplication.java
+++ src/main/java/com/javatechie/DevopsIntegrationApplication.java
@@ -11,7 +11,7 @@ public class DevopsIntegrationApplication {
 
 	@GetMapping
 	public String message(){
-		return "Welcome to our DevOps Project !!";
+		return "Welcome to Abdul's DevOps Project !!";
 	}
 
 	public static void main(String[] args) {
